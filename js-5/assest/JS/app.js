731b1a4236c5de0b4919c499e4dd105b4e8391c0
Ali Aliyev <106176887+im-Aliga@users.noreply.github.com>
2022-09-30 14:12:00 +0400

    Add files via upload

---
+++ js-5/assest/JS/app.js
@@ -0,0 +1,6 @@
+var xicon=document.querySelector("#xxxicon")
+var sidebar =document.querySelector(".sidebar")
+var exit= document.querySelector(".x")
+xicon.addEventListener('click',function(){
+   sidebar.classList.toggle('active')
+})
